02ca763aa82645cd92189e1491187b4e9373b799
Bayon Correa <bayron.correa.z@gmail.com>
2022-05-25 18:08:06 -0400

    practicando con modal provocando que el bundle.js pese menos

---
+++ src/components/modalSetting.js
@@ -0,0 +1,10 @@
+import Modal from "./modal";
+
+export default function ModalSetting({ title, root}){
+    return(
+        <Modal title={title} root={root} >
+            <h1>Hola que pasa</h1>
+        </Modal>
+    )
+
+}
